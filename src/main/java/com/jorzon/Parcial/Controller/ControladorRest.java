138f4e4314fd13f965a79ec99ec4b842782ef8f9
Jorzon07 <garzonsanchezjorgeivan@gmail.com>
2023-10-30 20:02:50 -0500

    Haciendolo paso a paso

---
+++ src/main/java/com/jorzon/Parcial/Controller/ControladorRest.java
@@ -0,0 +1,18 @@
+package com.jorzon.Parcial.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControladorRest {
+
+    @GetMapping("/")
+    public String saludar(){
+        return "Aqui va el texto que quiera mostrar";
+    }
+
+    @GetMapping("/about")
+    public String presentar(){
+        return "Aqui va la presentacion del creador :) ";
+    }
+}
